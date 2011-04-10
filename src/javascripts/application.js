a8d6511dfe0dac9aecf1c4e625f30126434539f5
Scott Williams <scott@krazyyak.com>
2011-04-09 20:11:11 -0700

    Added timer to top nav calls. Made top nav arial. Added drop shadow to self picture.

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -3,6 +3,10 @@ $(function() {
     $('#nav li a').click(function() {
         $('#nav li a').removeClass('selected');
         $(this).addClass('selected');
+        var $target = $(this.hash),
+            offset = $target.offset().top;
+        $('html,body').animate({scrollTop: offset}, 500);
+        return false;
     });
 
 });
