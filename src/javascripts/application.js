7f899f7a25741c5b7f823b52090dd726593e8e89
swilliams <scott@krazyyak.com>
2011-04-11 10:43:41 -0700

    Added nav logic to scrolling

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,12 +1,51 @@
 $(function() {
+    // adjust the nav 'selected' element on scroll.
+    var navElements = $('nav#nav li > a').map(function(i, n) {
+        var x = {
+            offset: $($(n).attr('href')).offset().top,
+            link: $(n)
+        };
+        return x;
+    });
+    var doScrollEvent = true,
+        currentNav = navElements[0].link;
+
+    function changeNav(selected) {
+        $('#nav li a').removeClass('selected');
+        selected.addClass('selected');
+        currentNav = selected;
+    }
 
     $('#nav li a').click(function() {
-        $('#nav li a').removeClass('selected');
-        $(this).addClass('selected');
+        changeNav($(this));
         var $target = $(this.hash),
             offset = $target.offset().top;
-        $('html,body').animate({scrollTop: offset}, 500);
+        doScrollEvent = false;
+        $('html,body').animate({scrollTop: (offset + 3)}, 500, 'swing', function() {
+            doScrollEvent = true;
+        });
         return false;
+    });
+
+    // preload images
+    var images = 4;
+    for (var i = 1; i < images + 1; i++) {
+        var img = new Image();
+        img.src = '/images/photo-' + i + '.jpg';
+    }
+
+    $(window).scroll(function(data) {
+        var pos = $(window).scrollTop();
+        if (pos === 0 || !doScrollEvent) {
+            return;
+        }
+        var results = $.grep(navElements, function(n, i) {
+            return pos > n.offset + 0;
+        });
+        var selected = results[results.length-1];
+        if (selected != currentNav) {
+            changeNav(selected.link);
+        }
     });
 
 });
